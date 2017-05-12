5ab6cd224709f19a177b9d0733d0349d6384b740
Stanley <stanleyjsdz@gmail.com>
2017-05-12 13:10:23 -0400

    fix example test to actually test something

--- __tests__/componentTests/NavigationTest.js
+++ __tests__/componentTests/NavigationTest.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter as Router } from 'react-router-dom';
+import muiTheme from '../../src/utils/Theme';
 import Navigation from '../../src/components/layout/Navigation';
 
-test('Navigation', () => {
-  it('initializes with correct state', () => {
-    const navigation = mount(<Navigation/>);
+injectTapEventPlugin();
 
-    expect(navigation.state()).toMatchSnapShot();
-  });
+const mountOptions = {
+  context: {muiTheme},
+  contextTypes: { muiTheme: PropTypes.object },
+  childContextTypes: { muiTheme: PropTypes.object }
+}
+
+const _navigation = (
+  <Router>
+    <Navigation />
+  </Router>
+);
+
+test('Navigation initializes with proper state', () => {
+  const navigation = mount(_navigation, mountOptions);
+
+  expect(navigation.find(Navigation).node.state).toMatchSnapshot();
+
+  const _2navigation = shallow(<Navigation />);
+  _2navigation.instance()._openUserMenu({currentTarget: "hello", preventDefault: () =>  null});
+
+  expect(_2navigation.state()).toMatchSnapshot();
 });
 
